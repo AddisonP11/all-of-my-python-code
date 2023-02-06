4cc95d151c2ccf2c30156626ba6b0b4e5cddad6f
AddisonP11 <124619014+AddisonP11@users.noreply.github.com>
2023-02-06 18:16:41 +0000

    bjjj

---
+++ project 2.py
@@ -0,0 +1,9 @@
+import math
+print("hello i am a calculator that will perform a baisc algebraic formula that is made with random numbers")
+#first step is the get your numbers from the random code wich will give you anything 1-100
+import random
+a = random.randint(1,100)
+b = random.randint(1,100)
+# now you want it to print what kind of result u want for example a/b
+result = a**2 + b**2
+print(result)
